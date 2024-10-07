b41796e20a57e4d0bc5b32c66eee52a7e94bfd7c
Guilherme <guifalcao2017@gmail.com>
2024-10-07 13:20:18 -0300

    Adição rota para editar tarefa

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -35,4 +35,21 @@ const Task = require('../models/tarefas')
     }
   });
 
+  router.put('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    const { titulo, descricao } = req.body;
+    try {
+      const tarefa = await Task.findByPk(id);
+      if (!tarefa) {
+        return res.status(404).json({ error: 'Tarefa não encontrada' });
+      }
+      tarefa.titulo = titulo;
+      tarefa.descricao = descricao;
+      await tarefa.save();
+      res.json(tarefa);
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao atualizar tarefa' });
+    }
+  });
+
 module.exports = router
